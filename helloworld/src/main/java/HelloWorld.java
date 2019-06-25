5a5dd31e114198901daa1f4b3a39cfb5889bf634
derekkinzo <Dhak241290>
2019-06-25 13:13:10 -0400

    Added screenshot and updated gitignore

--- helloworld/src/main/java/HelloWorld.java
+++ helloworld/src/main/java/HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
-
     public static void main(String[] args) {
         try {
-            System.out.println("Hello " + args[0] + "!");
+            System.out.println("Hello, " + args[0] + "!");
         } catch (Exception e) {
             System.out.println("Hello World!");
         }
