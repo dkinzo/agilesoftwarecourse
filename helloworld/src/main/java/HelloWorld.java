d3dfec03b6ecbe09f5a4b16033932d527df31e64
derekkinzo <Dhak241290>
2019-06-25 12:53:48 -0400

    Initial commit of main method and maven project

---
+++ helloworld/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello " + args[0] + "!");
+        } catch (Exception e) {
+            System.out.println("Hello World!");
+        }
+    }
+}
